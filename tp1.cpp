58755894133493065a7a09574976669048b0082a
FogozIV <43248155+FogozIV@users.noreply.github.com>
2021-03-05 15:45:55 +0100

    Replaced pointers type
    Didn't know we can use pointeur like this so I just changed it.

--- tp1.cpp
+++ tp1.cpp
@@ -4,8 +4,8 @@
 #include <string>
 #include <ctime>
 using namespace std;
-void gameloop(int *nn, int *nv, int *nt, bool is_rand);
-int askPickBall(int *nn, int *nv, int *nt, bool is_rand);
+void gameloop(int &nn, int &nv, int &nt, bool is_rand);
+int askPickBall(int &nn, int &nv, int &nt, bool is_rand);
 string getColorFromCode(int code);
 int main()
 {
@@ -18,55 +18,55 @@ int main()
 	cin >> tmp; 
 	is_auto = (tmp != 0);
 	while (nn < 1 || nn >20) {
-		cout << "Veuillez entrer le nombre de boule noire : "; //Loop that ask a valid amount of balls
+		cout << "Veuillez entrer le nombre de boule noire : "; //Loop that ask valid amount of balls
 		cin >> nn;
 		cout << endl;
 	}
-	while (nv < 1 || nv > 20) { //Loop that ask a valid amount of balls
+	while (nv < 1 || nv > 20) { //Loop that as valid amount of balls
 		cout << "Veuillez entrer le nombre de boule verte : ";
 		cin >> nv;
 		cout << endl;
 	}
 	int nt = nn + nv; //calculate the total amount of balls
 	for (int l = nt; l > 1; l--) { //loop that will itterate a given number of times proportional to the amount of balls
-		gameloop(&nn, &nv, &nt, is_auto); //Function working with pointeurs so that we can edit the value
+		gameloop(nn, nv, nt, is_auto); //Function working with pointeurs so that we can edit the value
 	}
 	cout << "Il reste uniquement une boule " << getColorFromCode(nv == 0) << endl; //Only to tell the final balls
 	return EXIT_SUCCESS;
 }
 //The code call when it loops it could be put in the loop but it's there to increase visibility not efficiency
-void gameloop(int *nn, int *nv, int *nt, bool is_rand) {
+void gameloop(int &nn, int &nv, int &nt, bool is_rand) {
 	bool picked = false;
 	int fPick = askPickBall(nn, nv, nt, is_rand); //first pick
 	int sPick = askPickBall(nn, nv, nt, is_rand); //second pick
-	(*nt) += 1;
+	nt++;
 	if (fPick == sPick) {
-		(*nn) += 1;
+		nn++;
 	}
 	else {
-		(*nv) += 1;
+		nv++;
 	}
 	cout << "Vous avez tirer une boule " << getColorFromCode(fPick) << " et une boule " << getColorFromCode(sPick) << ". Donc une boule " << getColorFromCode((fPick == sPick) ? 1 : 0) << " a été ajoutée." << endl; //Long line to display the picked colors and the result
 }
 //This is the logic behind the pick. This function has been created to avoid redundancy and to minimize the amount of lines.
-int askPickBall(int *nn, int *nv, int *nt, bool is_rand) {
+int askPickBall(int &nn, int &nv, int &nt, bool is_rand) {
 	while (true) { //AWFULL we should never use a while(true) but I guess it will be ok for this specific usage.
 		int input;
 		if (!is_rand) { //This function check if it's rand so that we can act the right way.
-			cout << "Quelle boule voulez-vous tirer (0 pour verte, 1 pour noire) ? Il reste : " << *nv << " boule(s) verte(s) et : " << *nn << " boule(s) noire(s)." << endl;
+			cout << "Quelle boule voulez-vous tirer (0 pour verte, 1 pour noire) ? Il reste : " << nv << " boule(s) verte(s) et : " << nn << " boule(s) noire(s)." << endl;
 			cin >> input;
 		}
 		else { 
 			input = rand() % 2; //This is the code for random pick this will return either 1 or 0.
 		}
-		if ((input == 0 && *nv == 0) || (input == 1 && *nn == 0) || (input !=0 && input!= 1)) continue; //A function that check if the selected value is valid
+		if ((input == 0 && nv == 0) || (input == 1 && nn == 0) || (input !=0 && input!= 1)) continue; //A function that check if the selected value is valid
 		if (input == 0) {//This will apply the main game logic
-			(*nv) -= 1;
+			nv--;
 		}
 		else {
-			(*nn) -= 1;
+			nn--;
 		}
-		(*nt) -= 1;
+		nt--;
 		return input;
 	}	
 }
